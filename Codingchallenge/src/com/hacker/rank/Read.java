77bc10b665e6907664a482efdbb88e38146b1962
Chan3562 <Chan3562@CSL0705.global.chainsys.com>
2024-06-26 10:22:47 +0530

    weird program done

---
+++ Codingchallenge/src/com/hacker/rank/Read.java
@@ -0,0 +1,18 @@
+package com.hacker.rank;
+
+import java.util.*;
+
+public class Read {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+       
+        while (scan.hasNext()) {
+            int a = scan.nextInt();
+            System.out.println(a);
+        }
+
+        scan.close();
+    }
+}
