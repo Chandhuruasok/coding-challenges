5928795c5b07ac9c41dbc1de3a60ed667fb057f2
Chan3562 <Chan3562@CSL0705.global.chainsys.com>
2024-07-15 17:48:28 +0530

    hashmap done

--- Codingchallenge/src/com/hacker/rank/Read.java
+++ Codingchallenge/src/com/hacker/rank/Read.java
@@ -6,7 +6,7 @@ public class Read {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        
        
         while (scan.hasNext()) {
             int a = scan.nextInt();
