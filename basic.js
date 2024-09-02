23c0ec1a28a7ac9d0077aa79c5bd06c997084be3
Chandhuruasok <110887146+Chandhuruasok@users.noreply.github.com>
2024-09-02 11:53:17 +0530

    Create basic.js

---
+++ basic.js
@@ -0,0 +1,10 @@
+<p id="one">Hello</p>
+<button onclick="result()">view</button>
+<script>
+var a =document.getElementById("one");
+console.log(a);
+function result()
+{
+	a.textContent="Hi";
+}
+</script>
